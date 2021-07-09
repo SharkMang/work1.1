d7f0df28241e360cc3cfb43d59a8045ccad4d5fe
SharkMan <you@example.com>
2021-07-09 17:48:19 +0300

    First try to add emit

---
+++ src/components/eventEmmiter.js
@@ -0,0 +1,23 @@
+class EventEmmiter {
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(eventName, callback) {
+    if( !this.events[eventName] ) {
+      this.events[eventName] = [];
+    }
+     
+    this.events[eventName].push(callback);
+    console.log(this.events);
+  }
+
+  unsubscribe(eventName, callback) {
+    this.events[eventName] = this.events[eventName].filter(eventCallback => callback !== eventCallback);
+  }
+
+  emit(eventName, args) {
+    const event = this.events[eventName];
+    event && event.forEach(callback => callback.call(null, args));
+  }
+}
